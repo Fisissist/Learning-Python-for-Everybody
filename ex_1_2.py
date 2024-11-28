53da062c3737008c047f125325cecb21c7681bc1
Fisissist <mae040@shsu.edu>
2024-11-28 01:23:12 -0600

    Exercises 1 and 2
    I learned how to open the terminal on mac and used codes ls, cd, ran the code using python3 "filename"
    Created variables and inputs and printed results

---
+++ ex_1_2.py
@@ -0,0 +1,26 @@
+"""
+Created on Nov 28 2024
+
+@author: Fisissist
+"""
+
+######### Exercise 1 ##########
+
+print("Exersice 1")
+
+# user input fuction
+nzt = input('Input name: ')
+
+# print("", user input)
+print("Hello",nzt)
+
+######### Exersice 2 ##########
+
+print("Exersice 2")
+
+# defining variable inputs
+xh = input('Enter Housrs: ')
+xr = input("Enter Rate: ")
+xp = float(xr) * float(xh)
+
+print("Pay: ", xp)
